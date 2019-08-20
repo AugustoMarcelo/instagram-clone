5740045db6234c7dbeb3790761ed8e166fdbea96
AugustoMarcelo <mrclgst10@gmail.com>
2019-08-19 22:06:43 -0300

    Adicionando loading

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,16 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import {View, FlatList} from 'react-native';
 
-import {Post, Header, Avatar, Name, PostImage, Description} from './styles';
+import {Post, Header, Avatar, Name, PostImage, Description, Loading} from './styles';
 
 export default function Feed() {
   const [feed, setFeed] = useState([]);
   const [page, setPage] = useState(1);
   const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
 
   async function loadPage(pageNumber = page) {
     if (total && pageNumber > total) return;
-    
+
+    setLoading(true);
+
     const response = await fetch(
       `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`,
     );
@@ -21,6 +24,7 @@ export default function Feed() {
     setTotal(Math.floor(totalItems / 5));
     setFeed([...feed, ...data]);
     setPage(pageNumber + 1);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -31,9 +35,10 @@ export default function Feed() {
     <View>
       <FlatList
         data={feed}
+        keyExtractor={post => String(post.id)}
         onEndReached={() => loadPage()}
         onEndReachedThreshold={0.1}
-        keyExtractor={post => String(post.id)}
+        ListFooterComponent={loading && <Loading />}
         renderItem={({item}) => (
           <Post>
             <Header>
